777f66e28af5980072a222ea2deab4d83e307e66
om-surushe <omsurushe1907@gmail.com>
2022-07-25 04:41:04 +0530

    idk

--- index.js
+++ index.js
@@ -7,25 +7,12 @@ app.listen(process.env.PORT || 3000,() => {
 
 app.use(express.static('public'))
 
-const { readFile, readFileSync } = require('fs');
-
-// app.get('/',(request,response) => {
-//     readFile('index.html','utf-8',(err,html) => {
-
-//         if(err){
-//             response.status(500).send("Sorry, Out of order");
-//         }
-
-//         response.send(html);
-
-//     })
-// })
 
 app.get('/', (req, res) => {
-    res.sendFile('./public/index.html');
+
+    res.sendStatus(200).sendFile('./public/index.html');
 })
 
 app.use('/favicon.ico', express.static('./download.jpg'));
 
-// index.js
 module.exports = app
