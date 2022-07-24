5c9cb5e2416555aa5b6e4e6af3583941807c629d
om-surushe <omsurushe1907@gmail.com>
2022-07-25 03:16:26 +0530

    yes

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ const {readFile} = require('fs').promises;
 app.get('/',async (request,response) => {
     response.send(await readFile('./home.html','utf-8'))
 });
-// app.use('/favicon.ico', express.static('images/favicon.ico'));
 app.use('/favicon.ico', express.static('./download.jpg'));
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`));
