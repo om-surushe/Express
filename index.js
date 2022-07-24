cd1e197385b5a51d9008482eab06c729372e6da4
om-surushe <omsurushe1907@gmail.com>
2022-07-25 04:17:46 +0530

    ha

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000,() => {
+    console.log("Running")
+});
 
-// app.use(express.static('public'))
+app.use(express.static('public'))
 
-const {readFile,readFileSync} = require('fs');
+const { readFile, readFileSync } = require('fs');
 
-app.get('/',(request,response) => {
-    readFile('index.html','utf-8',(err,html) => {
+// app.get('/',(request,response) => {
+//     readFile('index.html','utf-8',(err,html) => {
 
-        if(err){
-            response.status(500).send("Sorry, Out of order");
-        }
+//         if(err){
+//             response.status(500).send("Sorry, Out of order");
+//         }
 
-        response.send(html);
+//         response.send(html);
 
-    })
+//     })
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 })
 
 app.use('/favicon.ico', express.static('./download.jpg'));
